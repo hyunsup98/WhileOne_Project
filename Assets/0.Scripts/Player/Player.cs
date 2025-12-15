cd3b8368f83b70a50d7be885e96b5f14f231453d
Complex-Answer <zstep0101@gmail.com>
2025-12-15 11:54:44 +0900

    Feat : 플레이어 자체를 담는 스크립트 추가
    플레이어 상태를 관리하기 위해
    플레이어 스크립트를 하나 만들었습니다
    
    hp,attack,moveSpeed 등 여기서 작성을 하고 각 필요한 클래스에 프로퍼티로 전달해줄 예정입니다

--- Assets/0.Scripts/Player/Player.cs
+++ Assets/0.Scripts/Player/Player.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("플레이어 수치 조절 컴포넌트")]
+    [SerializeField] private float _hp;
+    [SerializeField] private float _stamina;
+    [SerializeField] private float _moveSpeed = 5f;
+
+    //외부에서 사용할 프로퍼티
+    public float Hp => _hp;
+    public float Stamina => _stamina;
+    public float MoveSpeed => _moveSpeed;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
