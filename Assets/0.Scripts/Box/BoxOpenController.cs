034b14dc3d451dce5464e470cc0275fcd2d8983c
KIM0UJU <donggongg@gmail.com>
2025-12-18 17:12:32 +0900

    [Feat]WeaponSpwaner 스크립트 추가
    WeaponSpwaner 스크립트 추가

--- Assets/0.Scripts/Box/BoxOpenController.cs
+++ Assets/0.Scripts/Box/BoxOpenController.cs
@@ -2,26 +2,16 @@ using UnityEngine;
 
 public class BoxOpenController : MonoBehaviour
 {
-    public GameObject boxUI;                         // 상자 UI
-    public Weapon.WeaponList[] boxWeaponList;        // 상자에서 나올 수 있는 무기 종류들
-    public float openChance = 0.2f;                  // 20% 확률로 무기 획득
+    public GameObject boxUI;              // 상호작용 UI
+    public float openChance = 0.2f;       // 무기 나올 확률
+    public WeaponSpawner weaponSpawner;   // 무기 생성 담당
 
     private bool isPlayerInRange = false;
-    private WeaponManager weaponManager;
-
-    void Start()
-    {
-        weaponManager = Object.FindAnyObjectByType<WeaponManager>();
-
-        if (weaponManager == null)
-        {
-            Debug.LogError("WeaponManager를 찾을 수 없습니다.");
-        }
-    }
+    private bool isOpened = false;        // 상자 1회용 처리
 
     void Update()
     {
-        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        if (isPlayerInRange && !isOpened && Input.GetKeyDown(KeyCode.E))
         {
             OpenBox();
         }
@@ -29,9 +19,11 @@ public class BoxOpenController : MonoBehaviour
 
     void OpenBox()
     {
+        isOpened = true;
+
         if (Random.value <= openChance)
         {
-            GetWeaponFromBox();
+            weaponSpawner.SpawnRandomWeapon();
         }
         else
         {
@@ -39,21 +31,6 @@ public class BoxOpenController : MonoBehaviour
         }
 
         boxUI.SetActive(false);
-    }
-
-    void GetWeaponFromBox()
-    {
-        if (boxWeaponList == null || boxWeaponList.Length == 0)
-        {
-            Debug.LogWarning("상자에 설정된 무기가 없습니다.");
-            return;
-        }
-
-        int randomIndex = Random.Range(0, boxWeaponList.Length);
-        Weapon.WeaponList selectedWeapon = boxWeaponList[randomIndex];
-
-        weaponManager.EquipWeapon(selectedWeapon);
-        Debug.Log("상자에서 무기 획득: " + selectedWeapon);
     }
 
     private void OnTriggerEnter(Collider other)
