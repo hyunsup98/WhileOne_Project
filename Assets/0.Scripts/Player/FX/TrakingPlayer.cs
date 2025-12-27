6a7788d8baaafc2942918f3dfcd4dd48d597d873
Complex-Answer <zstep0101@gmail.com>
2025-12-28 01:38:09 +0900

    Fix : 무기 이펙트 수정
    이펙트가 일관성없이 제멋대로 각도가 틀어지는 현상을 수정했습니다...만
    
    아직 약간의 오차가 있습니다

--- Assets/0.Scripts/Player/FX/TrakingPlayer.cs
+++ Assets/0.Scripts/Player/FX/TrakingPlayer.cs
@@ -28,7 +28,7 @@ public class TrakingPlayer : MonoBehaviour
         Vector3 _mouse = Mouse.current.position.ReadValue();
         float distanceToCamera = Mathf.Abs(Camera.main.transform.position.z);
         _mousePosition = Camera.main.ScreenToWorldPoint(new (_mouse.x, _mouse.y, distanceToCamera));
-        //_mousePosition.z = 0f;
+        _mousePosition.z = 0f;
 
         Vector2 dir = (_mousePosition - transform.position).normalized;
         float aaa = Mathf.Abs(Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
@@ -38,15 +38,16 @@ public class TrakingPlayer : MonoBehaviour
         if (transform.localScale.x > 0)
         {
             _attackFxInstance.transform.position = transform.position + (Vector3)(dir);
-            _attackFxInstance.transform.Rotate((Vector3)(dir));
+            _attackFxInstance.transform.right = -dir;
+            _rend.flipX = true;
         }
         else
         {
             _attackFxInstance.transform.position = transform.position + (Vector3)(dir);
-            _attackFxInstance.transform.Rotate((Vector3)(dir));
+            _attackFxInstance.transform.right = dir;
+            _rend.flipX = false;
         }
-        //_attackFxInstance.transform.position = _test.position + (Vector3)dir;
-        //_attackFxInstance.transform.rotation = _test.rotation;
+        
 
         _attackFxInstance.gameObject.SetActive(false);
         _attackFxInstance.gameObject.SetActive(true);
