cd8f3cad7ae2c72b6d8e31b322b48e1ea379a5fe
Hyun <shs217shs@naver.com>
2025-12-30 17:56:39 +0900

    Feat : 무기 효과음 찾아오는 메서드 추가

--- Assets/0.Scripts/System/Data/DataManager_SFX.cs
+++ Assets/0.Scripts/System/Data/DataManager_SFX.cs
@@ -1,6 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager_SFX : MonoBehaviour
 {
     [field: SerializeField] public SFXDatabaseSO SFXDatabase { get; private set; }
+
+    public string[] FindWeaponSound(string str)
+    {
+        List<string> sfxList = new List<string>();
+
+        foreach(var data in SFXDatabase.datas)
+        {
+            if(data.sfxID.Contains(str))
+            {
+                sfxList.Add(data.sfxID);
+            }
+        }
+
+        return sfxList.ToArray();
+    }
 }
